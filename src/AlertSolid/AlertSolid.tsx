a10e738b79f0aa14d87fb11a80b1d373493500a1
Atharva Taranekar <taranekaratharva@gmail.com>
2022-07-29 16:04:01 +0530

    removed default icons

--- src/AlertSolid/AlertSolid.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import createSvgIcon from "../utils/createSvgIcon";
-
-export default createSvgIcon(
-  <path d="M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2ZM11 16H13V18H11V16ZM12 6C12.5523 6 13 6.44772 13 7V13C13 13.5523 12.5523 14 12 14C11.4477 14 11 13.5523 11 13V7C11 6.44772 11.4477 6 12 6Z" />,
-  "AlertSolid"
-);
