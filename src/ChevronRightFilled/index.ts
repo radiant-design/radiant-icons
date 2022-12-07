c6a764c4869edc54bd517c1d9292e82fa53b2b4e
SPURGE <taranekaratharva@gmail.com>
2022-12-07 16:55:49 +0530

    Added ChevronUp ChevronLeft ChevronRight ChevronDown ProducTour Share ColumnSelector Aeroplane1 Filled icons

--- src/ChevronRightFilled/index.ts
+++ src/ChevronRightFilled/index.ts
@@ -1,1 +1,1 @@
-export { default } from "./ChevronRightFilled";
+export { default } from  "./ChevonRightFilled";
--- src/ChevronUpFilled/index.ts
+++ src/ChevronUpFilled/index.ts
@@ -1,1 +1,1 @@
-export { default } from  "./ChevonUpFilled";
+export { default } from  "./ChevronUpFilled";
